23d7abbd783b738b781349e9e8f846f79e4b4ae7
hach2adi <hachchadi.oumaima@gmail.com>
2024-04-19 18:30:09 +0100

    Dashboard Update

--- front-end/src/composants/DashboardPage.js
+++ front-end/src/composants/DashboardPage.js
@@ -18,58 +18,18 @@ import axios from "axios";
 import "../styles/DashboardPage.css";
 
 function DashboardPage() {
-  const [data, setData] = useState([
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ]);
-  const [pieChartData, setPieChartData] = useState([
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 },
-  ]);
+  const [data, setData] = useState([]);
 
-  const COLORS = ["#6ed18f"," #af98c5" ,"#2e7d32", "#9576eb"];
+  const COLORS = [
+    "#388e3c",
+    "#7e57c2",
+    "#43a047",
+    "#9c27b0",
+    "#4caf50",
+    "#9C27B0",
+    "#94e05e",
+    "#d975ea",
+  ];
 
   const RADIAN = Math.PI / 180;
   const renderCustomizedLabel = ({
@@ -81,9 +41,12 @@ function DashboardPage() {
     percent,
     index,
   }) => {
+    if (!pieData2[index]) return null;
+
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    const RADIAN = Math.PI / 180;
+    const x = cx + (outerRadius + 10) * Math.cos(-midAngle * RADIAN);
+    const y = cy + (outerRadius + 10) * Math.sin(-midAngle * RADIAN);
 
     return (
       <text
@@ -93,7 +56,7 @@ function DashboardPage() {
         textAnchor={x > cx ? "start" : "end"}
         dominantBaseline="central"
       >
-        {`${(percent * 100).toFixed(0)}%`}
+        {`${pieData2[index].name} (${(percent * 100).toFixed(0)}%)`}
       </text>
     );
   };
@@ -104,20 +67,95 @@ function DashboardPage() {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get("http://127.0.0.1:8000/api/gestion-stocks");
+      const response = await axios.get(
+        "http://127.0.0.1:8000/api/gestion-stocks"
+      );
       console.log(response.data);
       setData(response.data);
-      setPieChartData(response.data.map((item) => ({ name: item.name, value: item.value })));
     } catch (error) {
       console.error("Error fetching data:", error);
     }
+  };
+
+  const calculateSum = () => {
+    let sum = {
+      ammonix: 0,
+      tovex: 0,
+      detos: 0,
+      raccord_17: 0,
+      raccord_25: 0,
+      raccord_42: 0,
+      raccord_65: 0,
+      raccord_100: 0,
+      detonateur_450: 0,
+      detonateur_500: 0,
+      raccord: 0,
+      aei: 0,
+      ligne_tir: 0,
+    };
+
+    data.forEach((item) => {
+      sum.ammonix += parseInt(item.ammonix);
+      sum.tovex += parseInt(item.tovex);
+      sum.detos +=
+        parseInt(item.detonateur_450) +
+        parseInt(item.detonateur_500) +
+        parseInt(item.detos_500ms);
+      sum.raccord_17 += parseInt(item.raccord_17);
+      sum.raccord_25 += parseInt(item.raccord_25);
+      sum.raccord_42 += parseInt(item.raccord_42);
+      sum.raccord_65 += parseInt(item.raccord_65);
+      sum.raccord_100 += parseInt(item.raccord_100);
+      sum.detonateur_450 += parseInt(item.detonateur_450);
+      sum.detonateur_500 += parseInt(item.detonateur_500);
+      sum.aei += parseInt(item.aei);
+      sum.ligne_tir += parseInt(item.ligne_tir);
+      sum.raccord +=
+        parseInt(item.raccord_17) +
+        parseInt(item.raccord_25) +
+        parseInt(item.raccord_42) +
+        parseInt(item.raccord_65) +
+        parseInt(item.raccord_100);
+    });
+
+    return sum;
+  };
+
+  const sum = calculateSum();
+
+  const pieData1 = [
+    { name: "Ammonix", value: sum.ammonix },
+    { name: "Tovex", value: sum.tovex },
+  ];
+
+  const pieData2 = [
+    { name: "Raccord_17", value: sum.raccord_17 },
+    { name: "Raccord_25", value: sum.raccord_25 },
+    { name: "Raccord_42", value: sum.raccord_42 },
+    { name: "Raccord_65", value: sum.raccord_65 },
+    { name: "Raccord_100", value: sum.raccord_100 },
+    { name: "Detonateur_450", value: sum.detonateur_450 },
+    { name: "Detonateur_500", value: sum.detonateur_500 },
+    { name: "Aei", value: sum.aei },
+    { name: "Ligne_tir", value: sum.ligne_tir },
+  ];
+
+  const intialStock = {
+    ammonix: 3282900,
+    tovex: 45500,
+    detos: 87056,
+    raccord: 63608,
   };
 
   return (
     <main className="main-cont">
       <div
         className="main-title"
-        style={{ display: "flex", justifyContent: "space-between" }}
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          fontSize: "19px",
+        }}
       >
         <h3>DASHBOARD</h3>
       </div>
@@ -126,44 +164,58 @@ function DashboardPage() {
         <div className="card">
           <div className="card-inner">
             <h3>Ammonix</h3>
-            {/* <BsFillArchiveFill className='card_icon'/> */}
           </div>
-          <h1>300</h1>
+          <h1>{sum.ammonix}</h1>
+          <hr style={{ marginTop: "10px", marginBottom: "10px" }} />
+          <div className="card-inner">
+            <h3>Stock Initial</h3>
+          </div>
+          <h1>3282900</h1>
         </div>
         <div className="card">
           <div className="card-inner">
             <h3>Tovex</h3>
-            {/* <BsFillGrid3X3GapFill className='card_icon'/> */}
           </div>
-          <h1>12</h1>
+          <h1>{sum.tovex}</h1>
+          <hr style={{ marginTop: "10px", marginBottom: "10px" }} />
+          <div className="card-inner">
+            <h3>Stock Initial</h3>
+          </div>
+          <h1>45500</h1>
         </div>
         <div className="card">
           <div className="card-inner">
             <h3>Detos</h3>
-            {/* <BsPeopleFill className='card_icon'/> */}
           </div>
-          <h1>33</h1>
+          <h1>{sum.detos}</h1>
+          <hr style={{ marginTop: "10px", marginBottom: "10px" }} />
+          <div className="card-inner">
+            <h3>Stock Initial</h3>
+          </div>
+          <h1>87056</h1>
         </div>
         <div className="card">
           <div className="card-inner">
             <h3>Raccord</h3>
-            {/* <BsFillBellFill className='card_icon'/> */}
           </div>
-          <h1>42</h1>
+          <h1>{sum.raccord}</h1>
+          <hr style={{ marginTop: "10px", marginBottom: "10px" }} />
+          <div className="card-inner">
+            <h3>Stock Initial</h3>
+          </div>
+          <h1>63608</h1>
         </div>
       </div>
 
       <div className="charts">
-        <ResponsiveContainer width="100%" height="100%">
+        <ResponsiveContainer width="100%" height={300}>
           <BarChart
-            width={500}
-            height={500}
             data={data}
             margin={{
               top: 5,
               right: 30,
               left: 20,
-              bottom: 0,
+              bottom: 5,
             }}
           >
             <CartesianGrid strokeDasharray="3 3" />
@@ -171,21 +223,29 @@ function DashboardPage() {
             <YAxis />
             <Tooltip />
             <Legend />
-            <Bar dataKey="pv" fill="#8884d8" />
-            <Bar dataKey="uv" fill="#82ca9d" />
+            <Bar dataKey="ammonix" name="Ammonix" fill="#8884d8" />
+            <Bar dataKey="tovex" name="Tovex" fill="#82ca9d" />
+            <Bar dataKey="detonateur_500" name="Detos" fill="#9c27b0" />
+            <Bar dataKey="raccord_17" name="Raccord" fill="#43a047" />
           </BarChart>
         </ResponsiveContainer>
 
         <ResponsiveContainer width="100%" height="100%">
           <PieChart>
-            <Pie dataKey="value" data={pieChartData} fill="#8884d8" label />
+            <Pie dataKey="value" data={pieData1} fill="#8884d8" label>
+              {pieData1.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={index === 0 ? "#8884d8" : "#82ca9d"}
+                />
+              ))}
+            </Pie>
+            <Tooltip />
           </PieChart>
         </ResponsiveContainer>
 
-        <ResponsiveContainer width="100%" height="100%">
+        <ResponsiveContainer width="100%" height={300}>
           <LineChart
-            width={500}
-            height={300}
             data={data}
             margin={{
               top: 5,
@@ -200,34 +260,41 @@ function DashboardPage() {
             <Tooltip />
             <Legend />
             <Line
-              type="monotone"
-              dataKey="pv"
-              stroke="#8884d8"
+              type="tovex"
+              dataKey="tovex"
+              stroke="#6ED18F"
               activeDot={{ r: 8 }}
             />
-            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            <Line type="detos" dataKey="detonateur_450" stroke="#AF98C5" />
+            <Line type="detos" dataKey="detonateur_500" stroke="#56ffc6" />
+            <Line type="raccord" dataKey="raccord_17" stroke="#9576EB" />
+            <Line type="raccord" dataKey="raccord_25" stroke="#9C27B0" />
+            <Line type="raccord" dataKey="raccord_42" stroke="#b7b5f4" />
+            <Line type="raccord" dataKey="raccord_65" stroke="#94e05e" />
+            <Line type="raccord" dataKey="raccord_100" stroke="#d975ea" />
           </LineChart>
         </ResponsiveContainer>
 
         <ResponsiveContainer width="100%" height="100%">
           <PieChart width={400} height={161}>
             <Pie
-              data={pieChartData}
+              data={pieData2}
               cx="50%"
               cy="50%"
               labelLine={false}
               label={renderCustomizedLabel}
-              outerRadius={80}
+              outerRadius={120}
               fill="#8884d8"
               dataKey="value"
             >
-              {data.map((entry, index) => (
+              {pieData2.map((entry, index) => (
                 <Cell
                   key={`cell-${index}`}
                   fill={COLORS[index % COLORS.length]}
                 />
               ))}
             </Pie>
+            <Tooltip formatter={(value, name) => [name, value]} />
           </PieChart>
         </ResponsiveContainer>
       </div>
