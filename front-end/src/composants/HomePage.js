9577e57083bf633e71ff4322391eb8727a83a268
Ouiam7zagh <ouiamzaghou@gmail.com>
2024-04-24 07:22:27 +0100

    Home page

--- front-end/src/composants/HomePage.js
+++ front-end/src/composants/HomePage.js
@@ -1,63 +1,140 @@
-// HomePage.js
-
-import React from 'react';
-import '../styles/HomePage.css';
-import sautageImage from '../images/sautage.jpg';
-import sautageImage2 from '../images/sautage2.jpg';
-import tirImage from '../images/tir.jpg';
-import tirImage2 from '../images/tir2.jpg';
-import logoOcp from '../images/ocp.png';
+import React, { useState } from 'react';
+import '../styles/HomePage.css'; 
+import ammonix1Image1 from '../images/ammonix1.jpg';
+import ammonix2Image2 from '../images/ammonix2.jpg';
+import ammonix3Image3 from '../images/ammonix3.jpg';
+import tov1Image1 from '../images/tov1.jpg';
+import tov2Image2 from '../images/tov2.jpg';
+import tov3Image3 from '../images/tov3.jpg';
+import detos1Image1 from '../images/detos1.jpg';
+import detos2Image2 from '../images/detos2.jpg';
+import detos3Image3 from '../images/detos3.png';
+import racc1Image1 from '../images/racc1.jpg';
+import racc2Image2 from '../images/racc2.jpg';
+import racc3Image3 from '../images/racc3.jpg';
+import homeImage from '../images/home.jpg';
 
 const HomePage = () => {
+  const [currentDefinition, setCurrentDefinition] = useState(1);
+
+  const handleClick = (definitionNumber) => {
+    setCurrentDefinition(definitionNumber);
+  };
+
   return (
-    <div className="page-container">
-      <div className="homepage-container">
-        <div className="homepage-header">
-        <img src={logoOcp} alt="OCP Logo" className="logo" /> {/* Ajoutez le logo */}
-          <h1>Section Tir et Sautage</h1>
-          <p>Le sautage et le tir sont des processus essentiels dans l'industrie minière. Ils sont utilisés pour fragmenter la roche et faciliter l'extraction des minéraux.</p>
-        </div>
-        <div className="sautage-section">
-          <h2>Sautage</h2>
-          <div className="sautage-content">
-            <p>
-              Le sautage est un processus complexe qui comprend plusieurs étapes essentielles :
-            </p>
-            <ol>
-              <li>Planification : Cela implique l'identification des zones où des explosifs seront utilisés pour le sautage. Cette étape prend en compte la sécurité, l'efficacité et les impacts environnementaux.</li>
-              <li>Forage : Des trous sont percés dans la roche à l'aide de foreuses spéciales. La profondeur, l'espacement et l'angle des trous sont calculés en fonction de la géologie du site et de la taille désirée des fragments.</li>
-              <li>Chargement : Les trous de forage sont remplis d'explosifs, souvent sous forme de bâtonnets ou de gel explosif. Les explosifs sont généralement insérés dans les trous de forage à l'aide de dispositifs de chargement sécurisés.</li>
-              <li>Amorçage : Les détonateurs sont placés dans les explosifs et connectés à une ligne de détonation. Cette ligne relie tous les trous de forage et est utilisée pour synchroniser l'explosion.</li>
-              <li>Sécurisation : Avant de déclencher l'explosion, la zone est sécurisée pour s'assurer qu'aucune personne ou aucun équipement ne se trouve à proximité qui pourrait être endommagé par le sautage.</li>
-            </ol>
-            <div className="image-container">
-              <img src={sautageImage} alt="Sautage" className="sautage-image" />
-              <img src={sautageImage2} alt="Sautage 2" className="sautage-image" />
+    <div className="home-page">
+    <div className="description">
+    <h1 class="Title">Le stade de sautage</h1>
+      <p>le stade sautage est le deuxième étapes de la chaine cinématique d'éxploitation de phosphate qui se base sur l'utilisation<br/> 
+        des éxploisifs industrie pour la fragmentation du stéril afin d'être facile à enlever <br/>
+        au stade suivant (décapage).
+      
+      </p>
+    </div>
+      <img src={homeImage} alt="home" className="banner-image" /> 
+      <div className="definition-container">
+        <div className="definition">
+        <div className={currentDefinition === 1 ? '' : 'hidden'}>
+            <div className="definition-content">
+              <p className="definition-text">
+                <h1 className="definition-title"> Ammonix :</h1>
+             <b>Nom chimique :  </b>Nitrate d'ammonium<br/>
+             <b>Formule chimique :</b> NH₄NO₃ <br/>
+             <b>L'Ammonix </b>  est un sel blanc et inodore, soluble dans l'eau.  <br/>
+              Il est largement utilisé comme engrais azoté dans l'agriculture pour fournir de l'azote aux plantes sous forme d'ions nitrate et ammonium.  <br/>
+              Il est également utilisé dans l'industrie chimique comme matière première pour la synthèse de divers produits chimiques, et dans la production d'explosifs. <br/>
+	           <b>Explosifs : </b>En raison de sa capacité à libérer rapidement de grandes quantités de gaz lorsqu'il est décomposé par une explosion, l'Ammonix est également utilisé comme composant principal dans la fabrication d'explosifs civils et militaires, tels que le dynamite et le mélanges à base de nitrate d'ammonium. <br/>
+             <b>Sécurité : </b> En raison de son potentiel explosif, le stockage, <br/>
+             la manipulation et l'utilisation de l'Ammonix nécessitent des précautions spéciales pour éviter les accidents. <br/>
+             Des réglementations strictes sont en place dans de nombreux pays pour contrôler son utilisation et sa distribution afin de garantir la sécurité publique et environnementale.
+
+              </p>
+              <div className="image-container">
+                <div className="small-images">
+                  <img src={ammonix1Image1} alt="ammonix1" className="small-image" />
+                  <img src={ammonix2Image2} alt="ammonix2" className="small-image" />
+                  <img src={ammonix3Image3} alt="ammonix3" className="small-image" />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="tir-section">
-          <h2>Tir</h2>
-          <div className="tir-content">
-            <p>
-              Le tir est également un processus crucial avec les étapes suivantes :
-            </p>
-            <ol>
-              <li>Vérification de sécurité : Avant de déclencher l'explosion, la zone est de nouveau inspectée pour s'assurer qu'elle est sécurisée et que toutes les personnes ont été éloignées de la zone de danger.</li>
-              <li>Détonation : Les détonateurs sont activés, déclenchant ainsi l'explosion. La détonation se propage le long de la ligne de détonation, faisant exploser les charges explosives dans les trous de forage.</li>
-              <li>Fragmentation : L'explosion fragmente la roche en morceaux plus petits, ce qui facilite son extraction ultérieure.</li>
-              <li>Évaluation : Une fois l'explosion terminée, la zone est inspectée pour évaluer le succès du sautage et pour s'assurer qu'aucun problème de sécurité n'a été créé.</li>
-              <li>Nettoyage : Tout débris est éliminé et la zone est préparée pour les étapes suivantes de l'exploitation minière ou du traitement du phosphate.</li>
-            </ol>
-            <div className="image-container">
-              <img src={tirImage} alt="Tir" className="tir-image" />
-              <img src={tirImage2} alt="Tir 2" className="tir-image" />
+          <div className={currentDefinition === 2 ? '' : 'hidden'}>
+            <div className="definition-content">
+              <p className="definition-text">
+                <h1 className="definition-title"> Tovex :</h1>
+                <b>Tovex </b> est un explosif commercial utilisé principalement dans le domaine minier et de la démolition. <br/> 
+            Le nom TOVEX est dérivé de "TOrpille à haut Vitesse d'EXplosion". <br/> 
+            Il s'agit d'un explosif en émulsion, ce qui signifie qu'il est composé d'une émulsion d'eau dans de l'huile contenant un sensibilisateur et un agent de gonflement. <br/> 
+            Cette composition lui donne une grande stabilité et une puissance explosive élevée. <br/> 
+            <b>Les principales caractéristiques de TOVEX incluent : </b>
+                sa facilité d'utilisation. <br/> 
+                sa sécurité lors du stockage et de la manipulation. <br/> 
+                sa flexibilité d'application dans différents environnements et conditions de terrain. <br/> 
+              </p>
+              <div className="image-container">
+                <div className="small-images">
+                  <img src={tov1Image1} alt="Tovex1" className="small-image" />
+                  <img src={tov2Image2} alt="Tovex2" className="small-image" />
+                  <img src={tov3Image3}alt="Tovex3" className="small-image" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={currentDefinition === 3 ? '' : 'hidden'}>
+            <div className="definition-content">
+              <p className="definition-text">
+                <h1 className="definition-title"> Détonateur :</h1 > 
+                <b> Détonateur </b> est un dispositif utilisé dans les explosifs pour initier ou déclencher une réaction explosive contrôlée. <br/>
+                Il existe plusieurs types de détonateurs, chacun ayant ses propres caractéristiques et applications.<br/>
+                <b> Détonateur non-électrique (DNE) :</b>
+                 Les détonateurs non-électriques utilisent des mécanismes non-électriques pour amorcer l'explosion.
+              </p>
+              <div className="image-container">
+                <div className="small-images">
+                  <img src={detos1Image1} alt="Détonateur1" className="small-image" />
+                  <img src={detos2Image2} alt="Détonateur2" className="small-image" />
+                  <img src={detos3Image3} alt="Détonateur3" className="small-image" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={currentDefinition === 4 ? '' : 'hidden'}>
+            <div className="definition-content">
+              <p className="definition-text">
+                <h1 className="definition-title"> Raccords :</h1 > 
+                <b>Raccords</b> explosifs nonel sont des dispositifs utilisés dans le domaine de la démolition et de la construction pour relier les détonateurs aux charges explosives telles que le TNT, <br/>
+            le dynamite ou d'autres explosifs. <br/> 
+            <b> Types :</b> Il existe différents types de raccords explosifs nonel, notamment les simples, <br/>
+            les doubles et les triples, qui permettent de connecter un ou plusieurs détonateurs à une ou plusieurs charges explosives. <br/>
+            Les raccords peuvent également varier en longueur pour s'adapter aux besoins spécifiques de l'opération de démolition ou de fragmentation. <br/>
+            <b> Sécurité :</b> Les raccords explosifs nonel sont conçus avec des normes de sécurité strictes pour éviter tout déclenchement accidentel <br/>
+             et garantir la sécurité des opérateurs et du site. Ils sont souvent codés par couleur pour faciliter l'identification <br/>
+             et le suivi lors de l'installation et de l'utilisation. <br/>
+            <b> Utilisation :</b> Les raccords explosifs nonel sont largement utilisés dans diverses industries telles que la construction, <br/>
+            les mines, les carrières et la démolition pour effectuer des opérations de détonation contrôlée. <br/>
+            Leur flexibilité et leur fiabilité en font un choix populaire pour les professionnels de la démolition et de l'explosif.
+
+
+              </p>
+              <div className="image-container">
+                <div className="small-images">
+                  <img src={racc1Image1} alt="Raccord1" className="small-image" />
+                  <img src={racc2Image2} alt="Raccord2" className="small-image" />
+                  <img src={racc3Image3} alt="Raccord3" className="small-image" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <div className="nav-buttons">
+        <button onClick={() => handleClick(1)} className={currentDefinition === 1 ? 'active' : ''}>1</button>
+        <button onClick={() => handleClick(2)} className={currentDefinition === 2 ? 'active' : ''}>2</button>
+        <button onClick={() => handleClick(3)} className={currentDefinition === 3 ? 'active' : ''}>3</button>
+        <button onClick={() => handleClick(4)} className={currentDefinition === 4 ? 'active' : ''}>4</button>
+      </div>
     </div>
   );
-}
+};
 
 export default HomePage;
--- tir_sautage.sql
+++ tir_sautage.sql
@@ -10,9 +10,9 @@ INSERT INTO `commandes` (`date`, `Num_Commande`, `panneau`, `tranche`, `niveau`,
 
 
 INSERT INTO `couts` (`dateCommande`, `Num_Cout`, `cout_ammonix`, `cout_aei`, `cout_detos_500ms`, `cout_raccord_17`, `cout_raccord_25`, `cout_raccord_42`, `cout_raccord_65`, `cout_raccord_100`, `cout_detonateur_450`, `cout_detonateur_500`, `cout_tovex`, `ligne_tir`, `etat_stock`, `created_at`, `updated_at`) VALUES
-('2024-01-01', '1', 1349600, 0, 0, 131991, 5130, 14309, 0, 0, 81536, 181236, 52121, 0, '', NULL, NULL),
-('2024-02-01', '2', 2180325, 0, 0, 156631, 5584, 21044, 0, 0, 0, 357882, 69187, 0, '', NULL, NULL),
-('2024-03-01', '3', 2215150, 0, 0, 186191, 4013, 30083, 0, 0, 0, 465367, 88560, 0, '', NULL, NULL);
+('2024-01-01', '1', 1349600, 176, 0, 131991, 5130, 14309, 0, 0, 81536, 181236, 52121, 13125, '', NULL, NULL),
+('2024-02-01', '2', 2180325, 208, 0, 156631, 5584, 21044, 0, 0, 0, 357882, 69187, 15625, '', NULL, NULL),
+('2024-03-01', '3', 2215150, 200, 0, 186191, 4013, 30083, 0, 0, 0, 465367, 88560, 15625, '', NULL, NULL);
 
 
 
@@ -46,8 +46,8 @@ INSERT INTO `sautages` (`date`, `numero_execution`, `numero_commande`, `BLF_Ammo
 
 
 INSERT INTO `stock` (`date_commande`, `Num_Stock`, `ammonix`, `aei`, `detos_500ms`, `raccord_17`, `raccord_25`, `raccord_42`, `raccord_65`, `raccord_100`, `detonateur_450`, `detonateur_500`, `tovex`, `ligne_tir`, `etat_stock`, `created_at`, `updated_at`) VALUES
-('2024-01-01', '1', 192800, 0, 0, 3782, 147, 410, 0, 0, 1634, 3632, 2825, 0, '', NULL, NULL),
-('2024-02-01', '2', 311475, 0, 0, 4488, 160, 603, 0, 0, 0, 7172, 3750, 0, '', NULL, NULL),
-('2024-03-01', '3', 316450, 0, 0, 5335, 115, 862, 0, 0, 0, 9326, 4800, 0, '', NULL, NULL);
+('2024-01-01', '1', 192800, 22, 0, 3782, 147, 410, 0, 0, 1634, 3632, 2825, 10500, '', NULL, NULL),
+('2024-02-01', '2', 311475, 26, 0, 4488, 160, 603, 0, 0, 0, 7172, 3750, 12500, '', NULL, NULL),
+('2024-03-01', '3', 316450, 25, 0, 5335, 115, 862, 0, 0, 0, 9326, 4800, 12500, '', NULL, NULL);
 
 
